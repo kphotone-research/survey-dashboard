73f5724ecb8f5f4ce9d2b84b47defd414a25f0bc
kphotone-research <kphotoneresearch@gmail.com>
2025-04-10 16:58:59 +0530

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hpxcfhohoyjxyhfdxqlr.supabase.co'; // Your Supabase URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhweGNmaG9ob3lqeHloZmR4cWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyMTU0ODcsImV4cCI6MjA1OTc5MTQ4N30.S4EBc-WSIainf4En42v2TBHs-azE5-AVBOFGamxOCz4'; // Your Supabase API Key
+const supabaseUrl = process.env.SUPABASE_URL;  // Use environment variable for Supabase URL
+const supabaseKey = process.env.SUPABASE_KEY;  // Use environment variable for Supabase Key
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
-
